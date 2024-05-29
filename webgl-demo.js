829e5fa8c9d5559491f8de006dc65afc1aa8620d
Sathish Kannan <sathishkannan162@gmail.com>
2024-05-29 21:27:04 +0530

    fix: error due to glsl 3.0.0 not supported in my browser

--- webgl-demo.js
+++ webgl-demo.js
@@ -36,13 +36,14 @@ function main() {
   `;
   
 const fsSource = `
+    precision mediump float;
+
     varying highp vec2 vTextureCoord;
 
     uniform sampler2D uSampler;
-    out vec4 fragColor;
 
     void main(void) {
-      fragColor = texture(uSampler, vTextureCoord);
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
     }
   `;
 
